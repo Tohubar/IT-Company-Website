b663eab1c817c014bf1fa8898fd45d6b5f033581
Md. Tahuber Rahman <143600194+Tohubar@users.noreply.github.com>
2026-01-14 23:59:38 +0600

    Redesign homepage for Genius Zoo

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
         if($(window).scrollTop()>35)
         {
-            $('.header').css({'background':'#002e5f','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
+            $('.header').css({'background':'rgba(20, 11, 44, 0.95)','box-shadow':'0 .4rem 1rem rgba(0,0,0,.35)'});
         }
         else
         {
